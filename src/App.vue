8e7b9290cfcea2c869873145a0780e03cb439031
张文 <zhangw@2345.com>
2020-02-26 11:26:11 +0800

    文案修改

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home测试</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">主页</router-link> |
+      <router-link to="/about">关于我</router-link>
     </div>
     <router-view/>
   </div>
